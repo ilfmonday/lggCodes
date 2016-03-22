66d167d681f1dd5646c08b6c5a277989229c1d21
lgg <ilfmonday@qq.com>
2016-03-22 09:57:15 +0800

    #1	va_list的解析不能跨线程，会造成野指针，怀疑是线程栈里面的数据没有了，因此将主线程的回调放在宏里面执行。

--- common/block/LGGBlockObject.m
+++ common/block/LGGBlockObject.m
@@ -12,7 +12,7 @@ typedef void (^ObjAction)(id obj1);
 typedef void (^BoolAction)(BOOL suc);
 typedef void (^BoolObjAction)(BOOL suc, id obj1);
 
-#define BLOCK_CALL(blkType,action,...) if(action){ ( (blkType)action )(__VA_ARGS__);}
+#define BLOCK_CALL(blkType,action,...) if(action){ execOnMainThread(^{  ( (blkType)action )(__VA_ARGS__);  }); }
 
 @implementation LGGBlockObject
 
@@ -30,39 +30,27 @@ typedef void (^BoolObjAction)(BOOL suc, id obj1);
     va_list args;
     va_start(args,type);
     
-    [self p_dispathCallByType:type withArgs:args];
+    switch (type) {
+        case LGGBlockObjectParaType_Bool:
+            [self p_callBool:args];
+            break;
+            
+        case LGGBlockObjectParaType_Obj:
+            [self p_callObj:args];
+            break;
+            
+        case LGGBlockObjectParaType_BoolObj:
+            [self p_callBoolOjb:args];
+            break;
+            
+        default:
+            break;
+    }
     
-}
+    va_end(args);
+    DDLogInfo(@"[BlockObject]: call2.1 调用完毕");
 
--(void)p_dispathCallByType:(LGGBlockObjectParaType)type withArgs:(va_list)args
-{
-    DDLogInfo(@"[BlockObject]: call2.0 分发类型type %d, args:",(int)type);
     
-    execOnMainThread(^{
-        
-        switch (type) {
-            case LGGBlockObjectParaType_Bool:
-                [self p_callBool:args];
-                break;
-                
-            case LGGBlockObjectParaType_Obj:
-                [self p_callObj:args];
-                break;
-                
-            case LGGBlockObjectParaType_BoolObj:
-                [self p_callBoolOjb:args];
-                break;
-                
-            default:
-                break;
-        }
-        
-        //由于放到主线程了，需要等参数使用完毕后才能释放；如果放在callWithParas，可能存在提前释放问题
-        va_end(args);
-        
-        DDLogInfo(@"[BlockObject]: call2.0 调用完毕");
-        
-    });
 }
 
 -(void)p_callBool:(va_list)args
