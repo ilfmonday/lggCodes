8b9d94c51fbb930d53a6cca461eb3329bc7a2d88
lggzhou <ilfmonday@qq.com>
2018-08-06 16:49:03 +0800

    LikaiProj

---
+++ mac/interview/Likai_SichuanDaxue/Likai_SichuanDaxue/main.mm
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Likai_SichuanDaxue
+//
+//  Created by lgg on 2018/8/6.
+//  Copyright © 2018年 lgg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    }
+    NSLog(@"Hello, World!");
+    return 0;
+}
