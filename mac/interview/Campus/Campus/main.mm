d779076d349f0bf81b7f90b098e2eaa11882b7a7
lggzhou <ilfmonday@qq.com>
2018-08-06 17:14:46 +0800

    rename

--- mac/interview/Campus/Campus/main.mm
+++ mac/interview/Campus/Campus/main.mm
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  Likai_SichuanDaxue
+//  Campus
 //
 //  Created by lgg on 2018/8/6.
 //  Copyright © 2018年 lgg. All rights reserved.
@@ -8,17 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-struct ListNode {
-    int value;
-    ListNode* nextPtr;
-};
-
-void deleteCurrNode(ListNode* node)
-{
-    
-}
-
 int main(int argc, const char * argv[]) {
-    NSLog(@"Hello, World!");
     return 0;
 }
